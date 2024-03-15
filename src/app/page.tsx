8b7b9132de11ef45ad8cac072e74addbe0d6f54c
Yerovy Espitia <luyeesgar@gmail.com>
2024-03-15 15:40:55 -0500

    fix typo in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center p-24'>
       <section className='flex flex-col items-center gap-2 mb-10'>
         <h1 className='text-2xl font-bold'>Simulación</h1>
-        <h2>Generador congruencial lineal / mixta</h2>
+        <h2>Generador congruencial lineal / mixto</h2>
         <p>Presentado por: Andrés Álvarez y Luis Espitia</p>
       </section>
 
